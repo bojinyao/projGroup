6f70460c12ba6cddcbd734259f97d3eb8850a1bd
Max Yao <bojinyao@berkeley.edu>
2018-11-21 00:06:39 -0800

    have a pretty good idea where this is going. Ready to rest to start more serious work on Wed.

---
+++ part-1/final_solver.py
@@ -0,0 +1,100 @@
+import networkx as nx
+import os
+
+###########################################
+# Change this variable to the path to 
+# the folder containing all three input
+# size category folders
+###########################################
+path_to_inputs = "./inputs"
+
+###########################################
+# Change this variable if you want
+# your outputs to be put in a 
+# different folder
+###########################################
+path_to_outputs = "./outputs"
+
+def parse_input(folder_name):
+    '''
+        Parses an input and returns the corresponding graph and parameters
+
+        Inputs:
+            folder_name - a string representing the path to the input folder
+
+        Outputs:
+            (graph, num_buses, size_bus, constraints)
+            graph - the graph as a NetworkX object
+            num_buses - an integer representing the number of buses you can allocate to
+            size_buses - an integer representing the number of students that can fit on a bus
+            constraints - a list where each element is a list vertices which represents a single rowdy group
+    '''
+    graph = nx.read_gml(folder_name + "/graph.gml")
+    parameters = open(folder_name + "/parameters.txt")
+    num_buses = int(parameters.readline())
+    size_bus = int(parameters.readline())
+    constraints = []
+    
+    for line in parameters:
+        line = line[1: -2]
+        curr_constraint = [num.replace("'", "") for num in line.split(", ")]
+        constraints.append(curr_constraint)
+
+    return graph, num_buses, size_bus, constraints
+
+
+###########################################################
+# IMPORTANT Algorithms we can use to help partition the graph
+# Algorithm must return a set of nodes, and only require 
+# graph as input nothing else
+from networkx.algorithms import approximation
+
+"""
+Partition a graph using vertex cover algorithm
+@param graph: graph itself
+@param nodes: all nodes in the graph (optimization)
+@return a list of lists (of nodes) as a partition of the graph
+"""
+def vertex_cover_partition(graph, nodes):
+    prime_nodes = approximation.min_weighted_vertex_cover(graph)
+    return ''
+
+def dominating_set_partition(graph, nodes):
+    prime_nodes = nx.dominating_set(graph)
+    return ''
+
+algos = [vertex_cover_partition, dominating_set_partition]
+
+def solve(graph, num_buses, size_bus, constraints):
+    #TODO: Write this method as you like. We'd recommend changing the arguments here as well
+    nodes = set(graph.nodes)
+
+    if nx.is_connected(graph):
+        #TODO
+        for algorithm in algos: # GLOBAL var used here
+            partitions = algorithm(graph, nodes)
+
+    else:
+        all_components = nx.connected_components(graph)
+        num_components = nx.is_connected(graph)
+        for component in all_components:
+            return 
+
+
+
+
+
+def main():
+    size_categories = ['tiny']
+    for size in size_categories:
+        graph, num_buses, size_bus, constraints = parse_input(path_to_inputs + '/' + size)
+        solution = solve(graph, num_buses, size_bus, constraints)
+        output_file = open(path_to_outputs + "/" + size + ".out", "w")
+        for lst in solution:
+            output_file.write(str(lst))
+            output_file.write("\n")
+
+        output_file.close()
+
+
+main()
---
+++ part-1/tiny_graph_gen.py
@@ -0,0 +1,74 @@
+import networkx as nx
+import random as r
+import matplotlib.pyplot as plt
+
+graph_file_path = './inputs/tiny/graph.gml'
+constraints_path = './inputs/tiny/parameters.txt'
+
+k = 8       # num of buses
+s = 7       # capacity of each bus
+N = 20      # total num of students
+p_edge = 0.6    # probability of creating an edge in random graph  
+constraint_limit = 100  # upper limit on number of constraints
+
+
+"""
+Writing k, s, and constraints to the parameters file.
+"""
+def write_constraints(constraints, path, k, s):
+    f = open(path, "w+")
+    f.write(str(k) + "\n")
+    f.write(str(s) + "\n")
+    for lst in constraints:
+        f.write(str(lst))
+        f.write("\n")
+    f.close()
+
+"""
+Generates and writes graph to file
+Returns the Graph object for use
+"""
+def generate_graph(N):
+    G = nx.gnp_random_graph(N, p_edge)
+    nx.write_gml(G, graph_file_path)
+    return G
+
+
+"""
+Generates and writes constraints to file
+Returns the list of lists for use
+"""
+def generate_constraints(G, k, s):
+    constraints = []
+
+    for _ in range(constraint_limit):
+        rowdy_group_size = r.randint(2, s) # random number from 2 - capacity
+        curr_group = []
+        i = 0
+        while(i < rowdy_group_size):
+            curr_node = r.randint(0, N - 1)
+            if (curr_node not in curr_group):
+                curr_group.append(curr_node)
+                i = i + 1
+        constraints.append(curr_group)
+
+    write_constraints(constraints, constraints_path, k, s)
+    return constraints
+
+
+
+"""
+Perform sanity check on the generated outputs
+"""
+def sanity_check(N, G, constraints):
+    total_capacity = k * s >= N
+    if (not total_capacity):
+        print("total capacity not enough")
+    constraint_size = constraint_limit >= len(constraints)
+    return total_capacity and constraint_size
+
+
+G = generate_graph(N)
+constraints = generate_constraints(G, k, s)
+if (not sanity_check(N, G, constraints)):
+    print("Invalid stuff generated!")
